62b63071a7e58c986513d06358a344607fc743aa
wilmer-corrales-1992 <55849616+wilmer-corrales-1992@users.noreply.github.com>
2025-12-23 14:08:59 -0600

    remove site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wilmercorrales.github.io',
+
   vite: {
       plugins: [tailwindcss()],
   },
