604ddf7de8c64770e26edfef7fc46faa05ab45b0
Wilmer Corrales <wilmercorrales92@gmail.com>
2025-12-23 14:57:19 -0600

    change site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://wilmercorrales.github.io",
+  site: "https://wilmer-corrales-1992.github.io",
   base: "/",
   output: "static", 
   
